3ca7f34462ba296cb9ec71ab88cc2eab1dd3e030
kfinley <kyle@kylefinley.net>
2021-10-02 10:25:08 -0700

    initial commit

---
+++ src/bootstrapper.ts
@@ -0,0 +1,70 @@
+import 'reflect-metadata';
+import { S3Client } from '@aws-sdk/client-s3';
+import { SNSClient, } from "@aws-sdk/client-sns";
+import { SFNClient } from "@aws-sdk/client-sfn";
+import { Container } from 'inversify-props';
+import {
+  PublishMessageCommand,
+  StartStepFunctionCommand,
+  GetStoredObjectCommand
+} from "./index";
+import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
+import { CognitoIdentityProvider } from '@aws-sdk/client-cognito-identity-provider';
+import { AuthorizeCommand } from './authorize';
+
+export default function bootstrapper(container: Container) {
+
+  if (!container.isBound("CognitoIdentityClient")) {
+    container.bind<CognitoIdentityClient>("CognitoIdentityClient")
+      .toDynamicValue(() => new CognitoIdentityClient({
+        endpoint: "http://cloud-platform.cognito:9229"
+      }));
+  }
+
+  if (!container.isBound("CognitoIdentityProvider")) {
+    container.bind<CognitoIdentityProvider>("CognitoIdentityProvider")
+      .toDynamicValue(() => new CognitoIdentityProvider({
+        endpoint: "http://cloud-platform.cognito:9229"
+      }));
+  }
+
+  if (!container.isBound("SNSClient")) {
+    container.bind<SNSClient>("SNSClient")
+      .toDynamicValue(() => new SNSClient({
+        region: "us-east-1",
+        endpoint: "http://localhost:4002"
+      }));
+  }
+
+  if (!container.isBound("SFNClient")) {
+    container.bind<SFNClient>("SFNClient")
+      .toDynamicValue(() => new SFNClient({
+        endpoint: "http://cloud-platform.sfn:8083"
+      }));
+  }
+
+  if (!container.isBound("S3Client")) {
+    container.bind<S3Client>("S3Client")
+      .toDynamicValue(() => new S3Client({
+        region: "us-east-1",
+        endpoint: "http://localhost:4569",
+        forcePathStyle: true,
+        credentials: {
+          accessKeyId: 'S3RVER',
+          secretAccessKey: 'S3RVER',
+        }
+      }));
+  }
+
+  addTransientIfNeeded<AuthorizeCommand>(AuthorizeCommand, "AuthorizeCommand", container);
+  addTransientIfNeeded<GetStoredObjectCommand>(GetStoredObjectCommand, "GetStoredObjectCommand", container);
+  addTransientIfNeeded<PublishMessageCommand>(PublishMessageCommand, "PublishMessageCommand", container);
+  addTransientIfNeeded<StartStepFunctionCommand>(StartStepFunctionCommand, "StartStepFunctionCommand", container);
+}
+
+function addTransientIfNeeded<T>(constructor: any, id: string, container: Container) {
+  if (!container.isBound(id)) {
+    container.addTransient<T>(constructor, id);
+  }
+}
+
